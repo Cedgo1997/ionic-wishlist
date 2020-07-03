54334d30b2bc0fe5aa71377a14f37d6be5dd6d3e
Cesar Gonzalez <cedgo1997@gmail.com>
2020-07-03 18:24:06 -0400

    ngModel and ionChange added

--- src/app/pages/add/add.page.ts
+++ src/app/pages/add/add.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { WishesService } from "src/app/services/wishes.service";
+import { ActivatedRoute } from "@angular/router";
+import { List } from "src/app/models/list.model";
+import { ItemList } from 'src/app/models/list-item.model';
 
 @Component({
-  selector: 'app-add',
-  templateUrl: './add.page.html',
-  styleUrls: ['./add.page.scss'],
+  selector: "app-add",
+  templateUrl: "./add.page.html",
+  styleUrls: ["./add.page.scss"],
 })
 export class AddPage implements OnInit {
+  list: List;
+  itemName = "";
 
-  constructor() { }
+  constructor(
+    private wishService: WishesService,
+    private _route: ActivatedRoute
+  ) {
+    const listId = this._route.snapshot.paramMap.get("listId");
 
-  ngOnInit() {
+    this.list = this.wishService.getList(listId);
+    console.log(listId);
   }
 
+  ngOnInit() {}
+
+  addItem() {
+    if (this.itemName.length === 0) {
+      return;
+    }
+
+    const newItem = new ItemList(this.itemName);
+    this.list.items.push(newItem);
+    this.itemName = '';
+    this.wishService.saveStorage();
+  }
 }
