357da64f55fa1a8a60fa9ea162bfb1ceec8ab4a1
Cesar Gonzalez <cedgo1997@gmail.com>
2020-07-03 17:20:10 -0400

    saveStorage and loadStorage methods created and using.

--- src/app/services/wishes.service.ts
+++ src/app/services/wishes.service.ts
@@ -9,17 +9,27 @@ export class WishesService {
   lists: List[] = [];
 
   constructor() { 
-
-    const list1 = new List('Collect Infinity Stones');
-    const list2 = new List('Heroes to dissapear');
-
-    this.lists.push(list1, list2);
-
+    this.loadStorage();
   }
 
   addList(title:string) {
 
     const newList = new List(title);
     this.lists.push(newList);
+    this.saveStorage();
   }
+
+    saveStorage() {
+      localStorage.setItem('data', JSON.stringify(this.lists));
+    }
+
+    loadStorage() {
+      if (localStorage.getItem('data')) {
+        this.lists = JSON.parse(localStorage.getItem('data'));
+      } else {
+        this.lists = [];
+      }
+
+    }
+
 }
