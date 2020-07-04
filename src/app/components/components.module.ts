f66397d945ddb6ce9c4520835823a9bcd4e87c20
Cesar Gonzalez <cedgo1997@gmail.com>
2020-07-04 09:40:35 -0400

    exported listcomponent

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ListsComponent } from "./lists/lists.component";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ListsComponent],
+
+  exports: [ListsComponent],
+  imports: [CommonModule],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
